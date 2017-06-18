b45956513aa81a8b593fcb37b93d2a8ef79b5b24
KGH123 <znfmaltm1@naver.com>
2017-06-18 15:50:30 +0900

    지난 채용공고까지완료,,, 즐겨찾기 / 검색 / 닉네임 / 푸쉬알림 남았음...

--- app/src/main/java/org/kgh/skkucheerup/detail.java
+++ app/src/main/java/org/kgh/skkucheerup/detail.java
@@ -1,5 +1,6 @@
 package org.kgh.skkucheerup;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
@@ -12,6 +13,7 @@ import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -31,35 +33,109 @@ public class detail extends AppCompatActivity {
     private Animation translateLeftAnim2;
     private Animation translateRightAnim2;
 
+    int b=0;
+    LinearLayout container2;
+    private boolean allSelOn=false;
     String str;
     LinearLayout page2;
     ImageButton menuBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        page2 = (LinearLayout) findViewById(R.id.page2);
-        menuBtn = (ImageButton) findViewById(R.id.menuBtn);
+            page2 = (LinearLayout) findViewById(R.id.page2);
+            menuBtn = (ImageButton) findViewById(R.id.menuBtn);
 
-        translateLeftAnim2 = AnimationUtils.loadAnimation(this, R.anim.translate_left);
-        translateRightAnim2 = AnimationUtils.loadAnimation(this, R.anim.translate_right);
+            translateLeftAnim2 = AnimationUtils.loadAnimation(this, R.anim.translate_left);
+            translateRightAnim2 = AnimationUtils.loadAnimation(this, R.anim.translate_right);
 
-        SlidingPageAnimationListener2 animListener = new SlidingPageAnimationListener2();
-        translateRightAnim2.setAnimationListener(animListener);
-        translateLeftAnim2.setAnimationListener(animListener);
+            SlidingPageAnimationListener2 animListener = new SlidingPageAnimationListener2();
+            translateRightAnim2.setAnimationListener(animListener);
+            translateLeftAnim2.setAnimationListener(animListener);
 
-        super.onCreate(savedInstanceState);
-        setContentView(activity_detail);
-        page2 = (LinearLayout) findViewById(R.id.page2);
+            super.onCreate(savedInstanceState);
+            setContentView(activity_detail);
 
-        TextView comName=(TextView)findViewById(R.id.companyName);
-        TextView title=(TextView)findViewById(R.id.title);
-        TextView whichCam=(TextView)findViewById(R.id.whichCampus);
-        TextView content=(TextView)findViewById(R.id.content);
+            container2 = (LinearLayout) findViewById(R.id.container2);
 
-        Intent intent = getIntent();
-        comName.setText(intent.getStringExtra("companyName"));
-        title.setText(intent.getStringExtra("title"));
-        whichCam.setText(intent.getStringExtra("whichCam"));
-        content.setText(intent.getStringExtra("content"));
+            page2 = (LinearLayout) findViewById(R.id.page2);
+
+            TextView comName = (TextView) findViewById(R.id.companyName);
+            TextView title = (TextView) findViewById(R.id.title);
+            TextView whichCam = (TextView) findViewById(R.id.whichCampus);
+            TextView content = (TextView) findViewById(R.id.content);
+
+            Intent intent = getIntent();
+            comName.setText(intent.getStringExtra("companyName"));
+            title.setText(intent.getStringExtra("title"));
+            whichCam.setText(intent.getStringExtra("whichCam"));
+            content.setText(intent.getStringExtra("content"));
+
+        }
+
+    public void onPastClicked(View v){
+        Intent intent=new Intent(getApplicationContext(),PastActivity.class);
+        startActivity(intent);
+    }
+
+    public void onAllSelClicked(View v){
+        CheckBox b1 = (CheckBox) findViewById(R.id.bank);
+        CheckBox b2 = (CheckBox) findViewById(R.id.itt );
+        CheckBox b3 = (CheckBox) findViewById(R.id.media);
+        CheckBox b4 = (CheckBox) findViewById(R.id.mu);
+        CheckBox b5 = (CheckBox) findViewById(R.id.produce);
+        CheckBox b6 = (CheckBox) findViewById(R.id.service);
+        CheckBox b7 = (CheckBox) findViewById(R.id.you);
+        CheckBox b8 = (CheckBox) findViewById(R.id.bo);
+        CheckBox b9 = (CheckBox) findViewById(R.id.cons);
+        CheckBox b10= (CheckBox) findViewById(R.id.tong);
+        CheckBox b11= (CheckBox) findViewById(R.id.fore);
+        CheckBox b12= (CheckBox) findViewById(R.id.anso);
+
+            if (allSelOn == false) {
+                b1.setChecked(true);
+                b2.setChecked(true);
+                b3.setChecked(true);
+                b4.setChecked(true);
+                b5.setChecked(true);
+                b6.setChecked(true);
+                b7.setChecked(true);
+                b8.setChecked(true);
+                b9.setChecked(true);
+                b10.setChecked(true);
+                b11.setChecked(true);
+                b12.setChecked(true);
+                allSelOn=true;
+            }else{
+                b1.setChecked(false);
+                b2.setChecked(false);
+                b3.setChecked(false);
+                b4.setChecked(false);
+                b5.setChecked(false);
+                b6.setChecked(false);
+                b7.setChecked(false);
+                b8.setChecked(false);
+                b9.setChecked(false);
+                b10.setChecked(false);
+                b11.setChecked(false);
+                b12.setChecked(false);
+                allSelOn=false;
+            }
+    }
+
+    public void onSearchButton(View v){
+        if(b==0) {
+            LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            inflater.inflate(R.layout.search_sub, container2, true);
+            b++;
+        }
+        else{
+            LinearLayout con=(LinearLayout) findViewById(R.id.container2);
+            con.setVisibility(View.VISIBLE);
+        }
+    }
+
+    public void onSearchInvi(View v){
+        LinearLayout search=(LinearLayout) findViewById(R.id.container2);
+        search.setVisibility(View.INVISIBLE);
     }
 
     public void onmain1Clicked(View v){
